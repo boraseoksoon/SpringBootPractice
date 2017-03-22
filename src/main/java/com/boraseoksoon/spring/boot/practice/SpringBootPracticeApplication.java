f57f0a8ab6975d141bcc40adc656bfc0c2792ae6
Seoksoon Jang <boraseoksoon@gmail.com>
2017-03-23 02:35:24 +0900

    [DEPLOYMENT] the first deployment

---
+++ src/main/java/com/boraseoksoon/spring/boot/practice/SpringBootPracticeApplication.java
@@ -0,0 +1,12 @@
+package com.boraseoksoon.spring.boot.practice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootPracticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootPracticeApplication.class, args);
+	}
+}
